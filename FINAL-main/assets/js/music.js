ab567e6e11e98c5ece2867acb9a0d2d6254d64f1
nahaype14 <121814616+nahaype14@users.noreply.github.com>
2023-11-25 04:48:37 +0600

    Add files via upload

---
+++ FINAL-main/assets/js/music.js
@@ -0,0 +1,225 @@
+// ======================================================================
+// Music Player ==================================================================
+// ======================================================================
+
+let x = 0;
+
+$(window).scroll( () => {
+  if ($(".music-container").is(":visible") || x != 0) {
+    x += 1;
+    if (
+      $(window).scrollTop() + $(window).height() >
+      $(document).height() - 150
+    ) {
+      $(".music-container").fadeOut();
+    } else {
+      $(".music-container").fadeIn();
+    }
+  }
+});
+
+const musicContainer = document.getElementById("music-container");
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+
+const video = document.getElementById("video");
+const audio = document.getElementById("audio");
+const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
+const title = document.getElementById("title");
+const cover = document.getElementById("cover");
+const currTime = document.querySelector("#currTime");
+const durTime = document.querySelector("#durTime");
+
+// Song titles
+const songs = ["hey", "summer", "Syrlasu Valsi", "ukulele"];
+const disk = ["disk"];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+  title.innerText = song;
+  audio.src = `assets/music/${song}.mp3`;
+  cover.src = `assets/images/${disk[0]}.png`;
+}
+
+// Play song
+function playSong() {
+  musicContainer.classList.add("play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+
+  audio.play();
+  video.play();
+}
+
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove("play");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+
+  audio.pause();
+  video.pause();
+}
+
+// Previous song
+function prevSong() {
+  songIndex--;
+
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Next song
+function nextSong() {
+  songIndex++;
+
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime(e) {
+  const { duration, currentTime } = e.srcElement;
+  var sec;
+  var sec_d;
+
+  // define minutes currentTime
+  let min = currentTime == null ? 0 : Math.floor(currentTime / 60);
+  min = min < 10 ? "0" + min : min;
+
+  // define seconds currentTime
+  function get_sec(x) {
+    if (Math.floor(x) >= 60) {
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+          sec = Math.floor(x) - 60 * i;
+          sec = sec < 10 ? "0" + sec : sec;
+        }
+      }
+    } else {
+      sec = Math.floor(x);
+      sec = sec < 10 ? "0" + sec : sec;
+    }
+  }
+
+  get_sec(currentTime, sec);
+
+  // change currentTime DOM
+  currTime.innerHTML = min + ":" + sec;
+
+  // define minutes duration
+  let min_d = isNaN(duration) === true ? "0" : Math.floor(duration / 60);
+  min_d = min_d < 10 ? "0" + min_d : min_d;
+
+  function get_sec_d(x) {
+    if (Math.floor(x) >= 60) {
+      for (var i = 1; i <= 60; i++) {
+        if (Math.floor(x) >= 60 * i && Math.floor(x) < 60 * (i + 1)) {
+          sec_d = Math.floor(x) - 60 * i;
+          sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+        }
+      }
+    } else {
+      sec_d = isNaN(duration) === true ? "0" : Math.floor(x);
+      sec_d = sec_d < 10 ? "0" + sec_d : sec_d;
+    }
+  }
+
+  // define seconds duration
+
+  get_sec_d(duration);
+
+  // change duration DOM
+  durTime.innerHTML = min_d + ":" + sec_d;
+}
+
+// Event listeners
+playBtn.addEventListener("click", () => toggleSong());
+
+function toggleSong() {
+  const isPlaying = musicContainer.classList.contains("play");
+
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+}
+
+// Change song
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+
+// Time/song update
+audio.addEventListener("timeupdate", updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener("click", setProgress);
+
+// Song ends
+audio.addEventListener("ended", nextSong);
+
+// Time of song
+audio.addEventListener("timeupdate", DurTime);
+
+$(document).keydown(function (e) {
+  //For any other keypress event
+  if (e.which == 76 && e.altKey) {
+    toggleSong();
+  }
+});
+
+$(document).keydown(function (e) {
+  //For any other keypress event
+  if (e.which == 191 && e.altKey) {
+    x = 0;
+    $(".music-container").fadeToggle();
+  }
+});
+
+$(document).keydown(function (e) {
+  //For any other keypress event
+  if (e.which == 188 && e.altKey) {
+    prevSong();
+  }
+});
+$(document).keydown(function (e) {
+  //For any other keypress event
+  if (e.which == 190 && e.altKey) {
+    nextSong();
+  }
+});
---
+++ FINAL-main/assets/js/questions.js
@@ -0,0 +1,84 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "«Кітап» өлеңін кім жазған?",
+    options: [
+      "Ы. Алтынсарин",
+      "Ш. Құдайбердіұлы",
+      "Ғ. Мүсірепов",
+      "С. Бегалин",
+    ],
+    answer: "С. Бегалин",
+  },
+  {
+    numb: 2,
+    question: "B. Soqpaqbaev qai jyly düniege kelgen?",
+    answer: "1924 - 1991",
+    options: ["1924 - 1991", "1910 - 1982", "1873 - 1938", "1846 - 1945 "],
+  },
+  {
+    numb: 3,
+    question: "Säken Seifullin qai jyly tuğan?",
+    answer: "1894 - 1938",
+    options: ["1894 - 1938", "1845 - 1904", "1846 - 1945", "1668 - 1781"],
+  },
+  {
+    numb: 7,
+    question: "Taqyryp degenımız ne?",
+    options: [
+      "jazuşynyñ şyğarmasyndağy negızgı oiy",
+      "ädebi tuyndylar",
+      "şyğarma",
+      "öleñ",
+    ],
+    answer: "jazuşynyñ şyğarmasyndağy negızgı oiy",
+  },
+  {
+    numb: 5,
+    question:
+      "Aq kiımdı denelı, aq saqaldy, Soqyr myñqau tanymas tırı jandy. būl üzındı qai öleñnen alynğan?",
+    options: [
+      "A. Qūnanbaiūly «Qys»",
+      "B. Üsenbaev «Menıñ tuym»",
+      "K. Salyqov «Astana - Azianyñ aru qyzy»",
+      "B. Üsenbaev «Aiaz ata»",
+    ],
+    answer: "A. Qūnanbaiūly «Qys»",
+  },
+  {
+    numb: 6,
+    question: "«Qyzyl şūnaq» öleñınıñ avtory",
+    options: ["A. Qūnanbaiūly", "S. Seifullin", "M. Şahanov", "K. Salyqov"],
+    answer: "S. Seifullin",
+  },
+  {
+    numb: 7,
+    question: "A. Qūnanbaiūly qai jyly düniege keldı?",
+    answer: "1845 - 1904",
+    options: ["1894 - 1938", "1845 - 1904", "1846 - 1945", "1668 - 1781"],
+  },
+  {
+    numb: 8,
+    question: "Қ. Р. Мемлекеттік Әнұранның авторлары?",
+    options: [
+      "Ж. Нәжімеденов, Н. Ә. Назарбаев. Әнін: Ш. Қалдаяқов",
+      "Ы. Алтынсарин. әнін: Ш. Қалдаяқов",
+      "Н. Ә. Назарбаев",
+      "А. Құнанбаев, әнін Ш. Қалдаяқов",
+    ],
+    answer: "Ж. Нәжімеденов, Н. Ә. Назарбаев. Әнін: Ш. Қалдаяқов",
+  },
+  {
+    numb: 9,
+    question: "Ұстазы жақсының... жақсы.",
+    options: ["білімі", "өнері", "мінезі", "ұстамы"],
+    answer: "ұстамы",
+  },
+  {
+    numb: 10,
+    question: "A. Baitūrsynūly qai jyly düniege keldı?",
+    answer: "1872 - 1937",
+    options: ["1924 - 1992", "1910 - 1982", "1872 - 1937", "1846 - 1945 "],
+  },
+];
